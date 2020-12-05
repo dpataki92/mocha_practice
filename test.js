7dfbd4f48f1ea9b944d0a0af9ff23f1f89ef29c6
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-05 23:22:53 +0100

    add tests for function and object

--- test.js
+++ test.js
@@ -3,6 +3,16 @@ let expect = require('chai').expect;
 let name = 'React Quickly'
 let url = ['http://reactquickly.co', 'https://www.manning.com/books/react-quickly']
 
+const addNums = (a,b) => {
+    return a+b;
+}
+
+let obj = {
+    "firstProp": 1,
+    "secondProp": 2
+}
+
+
 describe('name and url', function() {
     it('must match the values', function(done){
       expect(name).to.be.a('string')
@@ -13,3 +23,13 @@ describe('name and url', function() {
       done()
     })
   })
+
+describe('function and object', function() {
+    it('must return the correct result and properties', function(done) {
+        let testAddition = addNums(10, 90);
+        expect(testAddition).to.equal(100);
+        expect(Object.keys(obj)).to.have.length(2);
+        expect(obj).to.have.deep.property('firstProp', 1);
+        done()
+    })
+})
